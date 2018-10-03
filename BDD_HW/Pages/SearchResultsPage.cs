cee883cb84137a0750394078780b13825825debb
dlinchenko <dlinchenko@corelogic.com>
2018-10-03 18:45:18 +0300

    added initial specflow and cleaned stuff

--- BDD_HW/Pages/SearchResultsPage.cs
+++ BDD_HW/Pages/SearchResultsPage.cs
@@ -12,64 +12,12 @@ namespace Pages
 
         internal SearchResultsPage(IWebDriver driver): base(driver) { }
 
-        /*[FindsBy(How = How.XPath, Using = "//input[@class='rz-header-search-input-text passive']")]
-        private IWebElement SearchField;
-
-        [FindsBy(How = How.CssSelector, Using = "#rz-search > form > span > span > button")]
-        private IWebElement SearchSubmitButton;*/
-
-
         [FindsBy(How = How.XPath, Using = "//div[@class='g-i-tile-i-title clearfix']")]
         private IWebElement FirstItemInSearchResults;
-
-        /*[FindsBy(How = How.XPath, Using = "//li[@name='characteristics']/a")]
-        private IWebElement CharacteristicsTab;
-
-
-        [FindsBy(How = How.XPath, Using = "//table[@class='chars-t']/tbody/*")]
-        private IWebElement CharacteristicsTable;
-
-        internal SearchResultsPage SearchItem (string query)
-        {
-            SearchField.SendKeys(query);
-            SearchSubmitButton.Click();
-            return this;
-        }*/
 
         internal void OpenFirstItemInSearchResults()
         {
             FirstItemInSearchResults.Click();
         }
-
-        /*internal SearchResultsPage OpenCharacteristicsTab()
-        {
-            CharacteristicsTab.Click();
-            return new SearchResultsPage(_driver);
-        }
-
-        internal Dictionary<string, IReadOnlyCollection<IWebElement>> GetItemCharacteristics()
-        {
-            var ret = new Dictionary<string, IReadOnlyCollection<IWebElement>> { };
-
-            var foo = CharacteristicsTable.FindElements(By.XPath("//tr"));
-
-            foreach (var c in CharacteristicsTable.FindElements(By.XPath("//tr")))
-            {
-                try
-                {
-                    ret.Add(c.FindElement(By.XPath("td/div[@class='chars-title']")).Text, c.FindElements(By.XPath("td/div[@class='chars-value']")));
-                }
-                catch (Exception e)
-                {
-                    if (e is NoSuchElementException || e is System.ArgumentException) { continue; }
-                    else { throw e.InnerException; }
-                }
-            }
-            return ret;
-        }*/
-
-
-
-
     }
 }
---
+++ BDD_HW/Tests/IphonePropsComparion.feature
@@ -0,0 +1,9 @@
+﻿Feature: IphonePropsComparion
+
+
+@mytag
+Scenario: Compare Iphones props
+	Given I am on Rozetka store page
+	When I search for Apple Iphone 7 and I open first found item
+	And I open characteristics tab
+	Then the result should be 120 on the screen
